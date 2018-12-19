fdd72cb96653a41f98e5583ab9bbe4d9776242c0
Masciooo <45816492+Masciooo@users.noreply.github.com>
2018-12-19 11:12:16 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -46,12 +46,12 @@
 		//i parametri sono cosa voglio mandare indietro al mio utente, rimando il testo che
 		//ho ricevuto e che si trova nella variabile $text
 		$parameters = array('chat_id' => $chatId, "text" => $text);
-		if($text=="data"){
+		if($text =="data"){
 			$text="la data è: ".date("d/m/y");
 			$parameters = array('chat_id' => $chatId, "text" => $text);
 		}
-		if($text=="saluto"){
-			$text="il saluto è: DUCE!";
+		if($text =="saluto"){
+			$text ="il saluto è: DUCE!";
 			$parameters = array('chat_id' => $chatId, "text" => $text);
 		}
 		//aggiungo il comando di invio
